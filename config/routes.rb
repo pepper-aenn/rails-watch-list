c1b5c3732af56495b0aa36b2a06d62bab25fd9c0
anna schäffner <anna-schaeffner@gmx.de>
2023-02-06 18:02:23 +0100

    routes and start of controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+  resources :lists, except: %w[edit update] do
+    resources :bookmarks, only: %w[new create]
+  end
+  resources :bookmarks, only: :destroy
 end
